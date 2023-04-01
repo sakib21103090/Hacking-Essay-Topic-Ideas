94ad68b772e1fd1a3613b71f5e7f41d4f9eaf146
sakib21103090 <21103090@iubat.edu>
2023-04-01 16:05:57 +0600

    time addition done

--- src/Components/SpentTime/SpentTime.jsx
+++ src/Components/SpentTime/SpentTime.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import './SpendTime.css'
 const SpentTime = ({cart2}) => {
     console.log(cart2)
+    let TotalTime=0;
+    for(const Hack of cart2)
+    TotalTime=TotalTime+parseInt(Hack.time);
     return (
         <div>
         <div>
-             {
-                cart2.map(b=>
-                    <p className='time-style card'>Spent time on read :0{b.time}min</p>
-                    )
-             }
-
+             <h4 className='style-time card'>Spent Time On Read : {TotalTime}min</h4>
         </div>
         </div>
     );
